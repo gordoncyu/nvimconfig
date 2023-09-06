00727908edf78e4a9af7274197a9c3252ec00f6e
Gordon Yu <gordoncyu@gmail.com>
2023-09-05 18:48:26 -0600

    paste from system clipboard

--- lua/gord/remap.lua
+++ lua/gord/remap.lua
@@ -16,6 +16,9 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+-- paste from
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("v", "<leader>p", "\"+p")
 
 -- Q is evil
 vim.keymap.set("n", "Q", "<nop>")
