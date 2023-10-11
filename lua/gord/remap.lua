1e1eb1ee11b716eb5ee45e2d39b45d8f94881a6f
Gordon Yu <gordoncyu@gmail.com>
2023-10-10 18:31:54 -0600

    s and x to sub and chmx

--- lua/gord/remap.lua
+++ lua/gord/remap.lua
@@ -34,5 +34,8 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- replace word in whole file
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>sub", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set("n", "<leader>chmx", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- gonil
+vim.keymap.set("n", "<leader>sgn", "oif err != nil {<CR>}<Esc>kA<CR>")
