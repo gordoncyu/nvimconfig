8b1e19516b20640ea2c680c6ef71d316f96b8546
Gordon <gordoncyu@gmail.com>
2023-12-01 13:42:43 -0700

    added <leader>dn and din, delete nested delete code nested below current cursor position level, or delete in nested for this level (including code above at the same level until less indentation is found)

--- lua/gord/remap.lua
+++ lua/gord/remap.lua
@@ -39,3 +39,55 @@ vim.keymap.set("n", "<leader>chmx", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- gonil
 vim.keymap.set("n", "<leader>sgn", "oif err != nil {<CR>}<Esc>kA<CR>")
+
+local function delete_nested()
+    local current_line = vim.api.nvim_win_get_cursor(0)[1]
+    local current_line_indent = vim.fn.indent(current_line)
+    local lines_in_buffer = vim.fn.line("$")
+
+    local next_line = current_line + 1
+    while true do
+        if next_line > lines_in_buffer then
+            break
+        end
+
+        local next_line_indent = vim.fn.indent(next_line)
+        local next_line_empty = vim.fn.getline(next_line) == ""
+
+        if not next_line_empty and next_line_indent <= current_line_indent then
+            break
+        end
+
+        vim.api.nvim_buf_set_lines(0, next_line - 1, next_line, false, {})
+        lines_in_buffer = lines_in_buffer - 1
+    end
+end
+
+-- delete all nested of greater indent
+vim.keymap.set("n", "<leader>dn", delete_nested, {noremap=true})
+-- delete all nested of equal indent of greater
+vim.keymap.set("n", "<leader>din", function ()
+    local current_line = vim.api.nvim_win_get_cursor(0)[1]
+    local current_line_indent = vim.fn.indent(current_line)
+
+    local prev_line = current_line - 1
+
+    while prev_line > 0 do
+        local prev_line_indent = vim.fn.indent(prev_line)
+        local prev_line_empty = vim.fn.getline(prev_line) == ""
+
+        if not prev_line_empty and prev_line_indent < current_line_indent then
+            break
+        end
+
+        prev_line = prev_line - 1
+    end
+
+    if prev_line == 0 then
+        return
+    end
+
+    vim.api.nvim_win_set_cursor(0, {prev_line, 0})
+
+    delete_nested()
+end, {noremap=true})
