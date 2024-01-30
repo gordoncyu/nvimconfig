9c489a43484f7b72aa241a999efe62af771714ce
Gordon <gordoncyu@gmail.com>
2024-01-30 13:21:35 -0700

    rainbow_csv.nvim

--- lua/gord/packer.lua
+++ lua/gord/packer.lua
@@ -45,7 +45,24 @@ return packer.startup({
             end
         })
 
-        use('lewis6991/gitsigns.nvim')
+        use('cameron-wags/rainbow_csv.nvim')
+
+        use({
+            'lewis6991/gitsigns.nvim',
+            config = function()
+                require('rainbow_csv').setup()
+            end,
+            ft = {
+                'csv',
+                'tsv',
+                'csv_semicolon',
+                'csv_whitespace',
+                'csv_pipe',
+                'rfc_csv',
+                'rfc_semicolon'
+            }
+        })
+
         use({
             'feline-nvim/feline.nvim',
             requires = {
