e2ebca57dd46bfdcb7a0ead3ce25480059646819
Gordon <gordoncyu@gmail.com>
2024-04-01 15:23:50 -0600

    disregard previous commit reasoning, will allow autopair rules until specific ones annoy me enough for me to disable them

--- after/plugin/nvim-autopairs.lua
+++ after/plugin/nvim-autopairs.lua
@@ -3,6 +3,14 @@ autopairs.setup({
     fast_wrap = {},
 })
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
+-- autopairs.remove_rule('(')
+-- autopairs.remove_rule('{')
+-- autopairs.remove_rule('[')
+autopairs.remove_rule('"""')
+-- autopairs.remove_rule('`')
+autopairs.remove_rule('```')
+
 local cmp = require('cmp')
 cmp.event:on(
   'confirm_done',
