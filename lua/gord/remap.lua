13d43b4ab27353de376ce0e681cbd30f45d9045c
gordoncyu <gordoncyu@gmail.com>
2024-07-30 13:57:45 -0600

    Remap <leader>chmx to just <leader>x

--- lua/gord/remap.lua
+++ lua/gord/remap.lua
@@ -82,7 +82,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- replace word in whole file
 vim.keymap.set("n", "<leader>sub", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
-vim.keymap.set("n", "<leader>chmx", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- gonil
 vim.keymap.set("n", "<leader>sgn", "oif err != nil {<CR>}<Esc>kA<CR>")
